c354752216e177b995a8c9fae7464a011a28ac8a
lturpaud <51416340+lturpaud@users.noreply.github.com>
2019-06-17 08:50:42 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function opaquenavbar(){
+	var winpos = document.getElementById("titbann").clientHeight;
+	if (.75 * winpos < window.pageYOffset) {
+		document.getElementById("navvbar").classList.add("navbarscroll");
+	}
+	else if (.75 * winpos >= window.pageYOffset) {
+		document.getElementById("navvbar").classList.remove("navbarscroll");
+	}
+}
+
+window.onscroll = function(){opaquenavbar()};
+
 
 var mousestick = 0;
 

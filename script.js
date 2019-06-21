82a42054633b8e046b9cde3b2b6bd507afe1bb3a
lturpaud <51416340+lturpaud@users.noreply.github.com>
2019-06-21 11:39:22 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ function opaquenavbar(){
 }
 
 window.onscroll = function(){opaquenavbar()};
+
+function scrollm(){
+	document.getElementById("m").scrollIntoView();
+	window.scrollBy(0, -50);
+}
+
+function scrolln(){
+	document.getElementById("n").scrollIntoView();
+	window.scrollBy(0, -50);
+}
+
+function scrollo(){
+	document.getElementById("o").scrollIntoView();
+	window.scrollBy(0, -50);
+}
+
+function scrollp(){
+	document.getElementById("p").scrollIntoView();
+	window.scrollBy(0, -50);
+}
+
+function scrollpoetbook(){
+	document.getElementById("poetbook").scrollIntoView();
+	window.scrollBy(0, -50);
+}
